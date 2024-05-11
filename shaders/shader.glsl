b1c17f79537365fa2dfe3b82b8d48d0f5ae852f8
Artur Maslobojev <arthurmasl@gmail.com>
2024-05-11 20:25:25 +0300

    shader

--- shaders/shader.glsl
+++ shaders/shader.glsl
@@ -6,35 +6,10 @@ in vec4 fragColor;
 out vec4 finalColor;
 
 uniform vec2 resolution;
-uniform float time = 0;
-
-vec3 palette(float t) {
-    vec3 a = vec3(0.5, 0.5, 0.5);
-    vec3 b = vec3(0.5, 0.5, 0.5);
-    vec3 c = vec3(1.0, 1.0, 1.0);
-    vec3 d = vec3(0.2, 0.4, 0.5);
-
-    return a + b * cos(6.28318 * (c * t + d));
-}
+uniform float time;
 
 void main() {
     vec2 uv = (gl_FragCoord.xy * 2.0 - resolution.xy) / resolution.y;
-    vec2 uv0 = uv;
-    vec3 final = vec3(0.0);
 
-    for (int i = 0; i < 4; i++) {
-        uv = fract(uv * 1.5) - 0.5;
-
-        float d = length(uv) * exp(-length(uv0));
-        vec3 col = palette(length(uv0) + i * 0.4 + time * 0.4);
-
-        d = sin(d * 8.0 + time) / 8.0;
-        d = abs(d);
-
-        d = pow(0.01 / d, 1.2);
-        col *= d;
-        final += col * d;
-    }
-
-    finalColor = vec4(final, 1.0);
+    finalColor = vec4(cos(uv.yx + time), pow(uv.y, 0.5), 1.0);
 }
